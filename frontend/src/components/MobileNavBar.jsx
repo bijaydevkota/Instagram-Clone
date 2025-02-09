b308572153d3cdce8daaba6fdc6f9501c0b75c4a
bijay <dbijay2058@gmail.com>
2025-02-09 16:49:59 +0545

    push

--- frontend/src/components/MobileNavBar.jsx
+++ frontend/src/components/MobileNavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function MobileNavBar() {
   return (
-    <div className='md:hidden flex justify-between px-4 mt-8'>
+    <div className='md:hidden flex justify-between px-4 mt-2 mb-2'>
         <div className='flex items-center'>
         <img height={140} width={140} src="/insta.png" alt="" />
         <ChevronDown />
